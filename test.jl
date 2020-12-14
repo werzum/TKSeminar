81bcea3d84e5433c26426b9cef74a894dcf87514
Carl Orge Retzlaff <e.awerg@web.de>
2020-12-14 22:13:10 +0100

    added RT dataframe with the full text corresponding to the tweets

--- test.jl
+++ test.jl
@@ -1,23 +1,59 @@
-using CSV, DataFrames, LightGraphs, MetaGraphs, GraphPlot, Plots, StatsPlots, PackageCompiler, Dictionaries, Distributed, Dates
+using CSV, DataFrames, LightGraphs, MetaGraphs, GraphPlot, Plots, StatsPlots
+using PackageCompiler, Dictionaries, Distributed, Dates, JSON3, JSON
+
 #load dataframe and find a certain ID
 #df_ids = DataFrame!(CSV.File("df_ids.csv"))
 df_en = DataFrame!(CSV.File("df_en.csv"))
+const df_en_const = df_en[1:1000000,:]
+
+#read the highest RT texts
+tweet_dict = JSON.parsefile("tweets_RT_UTF8.txt")
+#create dict with the texts
+tweet_ID_text_dict = Dict()
+for elm in tweet_dict
+    tweet_ID_text_dict[elm["id"]] = elm["full_text"]
+end
+#and add it to the RT df
+insertcols!(c,2, "full_text" => ["" for i in nrow(c)])
+for row in eachrow(c)
+    try
+        row."full_text" = tweet_ID_text_dict[row.Id]
+    catch
+    end
+end
+#save the DF
+#CSV.write("df_RT.csv",c)
+
 #added an index since accessing the row index during iteration seems impossible (or well hidden)
 # CSV.write("df_en.csv",df_en)
 #df_en.:index = collect(1:nrow(df_en))
 const df_en_const = df_en[1:1000000,:]
 
-#select the million most retweeted messages
-df_rts = sort!(df_en, (:"Retweet-Count"))
-df_rts = df_rts[end-1000000:end,:]
+function create_selected_dfs(df_en)
+    #select the million most retweeted messages
+    df_rts = sort!(df_en, (:"Retweet-Count"))
+    df_rts = df_rts[end-1000000:end,:]
 
-#select each nth row to reduce the dataframe to 1 million tweets
-df_1ml = filter(row->(row.:index%21)==0,df_en)
+    #select each nth row to reduce the dataframe to 1 million tweetssoun
+    df_1ml = filter(row->(row.:index%21)==0,df_en)
 
-#select the million most polarized tweets
-df_pol = sort!(df_en, (:"Score"))
-df_pol = vcat(df_pol[1:500000,:],df_pol[end-500000:end,:])
+    #select the million/100.000 most polarized tweets
+    df_pol = sort!(df_en, (:"Score"))
+    df_pol = vcat(df_pol[1:50000,:],df_pol[end-50000:end,:])
 
+    return df_rts,df_1ml,df_pol
+end
+
+a,b,c = create_selected_dfs(df_en)
+
+
+create_selected_dfs(df_en)
+#TODO: IDEAS for analyzing
+#use of hashtag and negativity/positivity
+#most active users (most from)-> what did they spread?
+#most influential users (most RTs) -> what did they spread?
+#network with most RTetd content
+#color positivity/negativity in responses
 
 function create_graph(df_en)
     meta_graph = MetaGraph(SimpleGraph())
@@ -37,22 +73,32 @@ function create_graph(df_en)
     return meta_graph
 end
 
+temp_graph = create_graph(df_en_const)
+centralities(temp_graph)
+
+display(StatsPlots.histogram(degree_histogram(temp_graph),yaxis=(:log10), bins=150))
+print("global cc is $(global_clustering_coefficient(temp_graph))")
+
 @time create_graph(df_en_const)
 
 function centralities(meta_graph)
     #show a degree histogram
-    display(StatsPlots.histogram(degree_histogram(meta_graph)))
+    display(StatsPlots.histogram(degree_histogram(temp_graph),yaxis=(:log10), bins=150))
+
+    #these are too slow for 1ml networks
     #print the global cc
-    print("global cc is $(global_clustering_coefficient(meta_graph))")
+    #print("global cc is $(global_clustering_coefficient(meta_graph))")
     #and the betweenness with 0s removed to shrink the graph somewhat
-    betweenness = betweenness_centrality(meta_graph)
-    betweenness_filtered = filter(x->x!=0, betweenness)
-    display(histogram(betweenness_filtered))
+    # betweenness = betweenness_centrality(meta_graph)
+    # betweenness_filtered = filter(x->x!=0, betweenness)
+    # display(histogram(betweenness_filtered))
 end
 
 function plot_graph(meta_graph)
     display(gplot(meta_graph))
 end
+
+function top_hashtags(df_en)
 
 #which functions?
 #for each x days in timeframe
@@ -80,6 +126,3 @@ function for_x_days(x,df_en)
     end
     end
 end
-#
-# function create_2_week_graphs(df_en, unique_ids_dict)
-#     start_date = df_en[1,"Created-At"]
